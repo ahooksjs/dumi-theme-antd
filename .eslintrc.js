bf58921d26f1df0b0e68e8fee7279cdda90df320
liuyib <1656081615@qq.com>
2025-11-14 17:20:10 +0800

    fix: path error in dist

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/no-empty-function': 0,
     '@typescript-eslint/no-non-null-asserted-optional-chain': 0,
-    '@typescript-eslint/no-non-null-assertion': 0
+    '@typescript-eslint/no-non-null-assertion': 0,
+    'jsx-a11y/control-has-associated-label': 0
   }
 };
